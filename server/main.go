284bcb2afc85dc18343daa05c259bfaff712d27a
Domas Monkus <domas@monkus.lt>
2020-01-10 07:33:17 +0200

    Remove stale dependency.

--- server/main.go
+++ server/main.go
@@ -8,10 +8,8 @@ package main
 
 import (
 	"github.com/mattermost/mattermost-server/v5/plugin"
-
-	"github.com/tasdomas/mattermost-plugin-nop/hooks"
 )
 
 func main() {
-	plugin.ClientMain(&hooks.NopPlugin{})
+	plugin.ClientMain(&NopPlugin{})
 }
